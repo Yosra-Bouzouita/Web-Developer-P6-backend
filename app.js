2db86209c5f50f40448f896120466e132cf49145
Yosra Chebbi <yosrabouzouita16@gmail.com>
2021-11-19 01:07:41 +0100

    changer de format

--- app.js
+++ app.js
@@ -1,27 +1,35 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const path = require('path');
-const saucesRoutes = require('./routes/sauces');
-const userRoutes = require('./routes/user');
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const saucesRoutes = require("./routes/sauces");
+const userRoutes = require("./routes/user");
 
-mongoose.connect('mongodb+srv://mohamedali:Yosra1986@cluster0.hqxei.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-  { useNewUrlParser: true,
-    useUnifiedTopology: true })
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
+mongoose
+  .connect(
+    "mongodb+srv://mohamedali:Yosra1986@cluster0.hqxei.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
 
 const app = express();
 
 app.use(express.json());
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-  });
-  app.use('/images', express.static(path.join(__dirname, 'images')));
-  app.use('/api/sauces', saucesRoutes);
-  app.use('/api/auth', userRoutes);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+app.use("/images", express.static(path.join(__dirname, "images")));
+app.use("/api/sauces", saucesRoutes);
+app.use("/api/auth", userRoutes);
 
-  module.exports = app;
+module.exports = app;
